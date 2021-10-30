dc32b44c307679c82cd18d946e84ba7c4ff72088
LevyvoNet <eladlevy2@gmail.com>
2021-10-30 19:16:31 +0300

    reverted again, sanity check

--- gym_mapf/mapf_env/mapf_env.h
+++ gym_mapf/mapf_env/mapf_env.h
@@ -16,7 +16,7 @@ using namespace std;
 
 /** Consts ***********************************************************************************************************/
 #define N_PRIMES (25)
-
+uint8_t g_primes[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 
 class MultiAgentState {
 public:
@@ -34,15 +34,14 @@ public:
 };
 
 inline size_t hash<MultiAgentState>::operator()(const MultiAgentState &s) const {
-    uint8_t primes[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-    double h = 0;
+    size_t h = 0;
     int i = 0;
 
     for (i = 0; i < s.locations.size(); i++) {
-        h += pow(primes[i % N_PRIMES], hash<Location>()(s.locations[i]));
+        h += pow(g_primes[i % N_PRIMES], hash<Location>()(s.locations[i]));
     }
 
-    return hash<double>()(h);
+    return h;
 }
 
 /* Forward declaration */
@@ -102,13 +101,12 @@ public:
 };
 
 size_t hash<MultiAgentAction>::operator()(const MultiAgentAction &a) const {
-    uint8_t primes[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
-    double h = 0;
+    size_t h = 0;
     for (size_t i = 0; i < a.actions.size(); ++i) {
-        h += pow(primes[i % N_PRIMES], (int) a.actions[i]);
+        h += pow(g_primes[i % N_PRIMES], (int) a.actions[i]);
     }
 
-    return hash<double>()(h);
+    return h;
 }
 
 
