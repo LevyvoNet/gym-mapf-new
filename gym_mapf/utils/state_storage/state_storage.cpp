b83eb36879193d43cbcca717beed59e779ee3cfa
LevyvoNet <eladlevy2@gmail.com>
2021-11-01 15:10:59 +0200

    maybe even more

--- gym_mapf/utils/state_storage/state_storage.cpp
+++ gym_mapf/utils/state_storage/state_storage.cpp
@@ -6,27 +6,30 @@
 
 /** Hash functions implementations *******************************************************************************/
 size_t std::hash<Location>::operator()(const Location &l) const {
-    return pow(2, l.row) + pow(3, l.col);
+//    return pow(2, l.row) + pow(3, l.col);
+    return std::hash<int64_t>()((long) l.id);
 }
 
 size_t hash<MultiAgentState>::operator()(const MultiAgentState &s) const {
-    size_t h = 0;
-    int i = 0;
-
-    for (i = 0; i < s.locations.size(); i++) {
-        h += i ^ hash<Location>()(s.locations[i]);
-    }
-
-    return h;
+//    size_t h = 0;
+//    int i = 0;
+//
+//    for (i = 0; i < s.locations.size(); i++) {
+//        h += i ^ hash<Location>()(s.locations[i]);
+//    }
+//
+//    return h;
+    return std::hash<int64_t>()((long) s.id);
 }
 
 size_t hash<MultiAgentAction>::operator()(const MultiAgentAction &a) const {
-    size_t h = 0;
-    for (size_t i = 0; i < a.actions.size(); ++i) {
-        h += i ^ a.actions[i];
-    }
-
-    return h;
+//    size_t h = 0;
+//    for (size_t i = 0; i < a.actions.size(); ++i) {
+//        h += i ^ a.actions[i];
+//    }
+//
+//    return h;
+    return std::hash<int64_t>()((long) a.id);
 }
 
 
