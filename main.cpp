508708f554fc2e915ccbbdee96cff631834d3364
LevyvoNet <eladlevy2@gmail.com>
2021-11-02 11:53:57 +0200

    having few benchmarks

--- main.cpp
+++ main.cpp
@@ -7,47 +7,159 @@
 #include <gym_mapf/gym_mapf.h>
 #include <solvers/value_iteartion/value_iteration.h>
 
-int main(int argc, char **argv) {
-    MapfEnv *env = create_mapf_env("empty-16-16", 1, 2, 0.2, -1000, 0, -1);
+/** Abstracts ********************************************************************************************************/
+class EnvCreator {
+public:
+    string name;
 
-    ValueIterationPolicy policy = ValueIterationPolicy(env, 1.0, "vi");
+    EnvCreator(string name) : name(name) {}
 
-    policy.train();
-    TrainInfo *train_info = policy.get_train_info();
-    EvaluationInfo *eval_info = policy.evaluate(100, 1000, 0);
+    virtual MapfEnv *operator()() = 0;
+};
+
+class SolverCreator {
+public:
+    virtual Policy *operator()(MapfEnv *env, float gamma) = 0;
+};
+
+
+/** Envs **********************************************************************************************************/
+class EmptyGrid : public EnvCreator {
+public:
+    size_t n_agents;
+    size_t grid_size;
+    int goal_reward;
+
+    EmptyGrid(string name, size_t grid_size, size_t n_agents, int goal_reward) : EnvCreator(name), grid_size(grid_size),
+                                                                                 n_agents(n_agents),
+                                                                                 goal_reward(goal_reward) {}
+
+    virtual MapfEnv *operator()() {
+        std::ostringstream map_name;
+        map_name << "empty-" << this->grid_size << "-" << this->grid_size;
+
+        return create_mapf_env(map_name.str(), 1, this->n_agents, 0.2, -1000, this->goal_reward, -1);
+    }
+};
+
+
+class SymmetricalBottleneck : public EnvCreator {
+public:
+    int goal_reward;
+
+    SymmetricalBottleneck(string name, int goal_reward) : EnvCreator(name), goal_reward(goal_reward) {}
+
+    virtual MapfEnv *operator()() {
+        vector<std::string> map_lines({
+                                              "..@...",
+                                              "..@...",
+                                              "......",
+                                              "..@...",
+                                              "..@..."
+                                      });
+
+        Grid *g = new Grid(map_lines);
+
+        return new MapfEnv(g,
+                           2,
+                           {g->get_location(2, 0), g->get_location(2, 5)},
+                           {g->get_location(2, 5), g->get_location(2, 0)},
+                           0.2,
+                           -1000,
+                           goal_reward,
+                           -1);
+
+    }
+
+
+};
+
+class ASymmetricalBottleneck : public EnvCreator {
+public:
+    int goal_reward;
+
+    ASymmetricalBottleneck(string name, int goal_reward) : EnvCreator(name), goal_reward(goal_reward) {}
+
+    virtual MapfEnv *operator()() {
+        vector<std::string> map_lines({
+                                              "..@..",
+                                              "..@..",
+                                              ".....",
+                                              "..@..",
+                                              "..@.."
+                                      });
+
+        Grid *g = new Grid(map_lines);
+
+        return new MapfEnv(g,
+                           2,
+                           {g->get_location(2, 0), g->get_location(2, 5)},
+                           {g->get_location(2, 5), g->get_location(2, 0)},
+                           0.2,
+                           -1000,
+                           goal_reward,
+                           -1);
+
+    }
+
+
+};
+
+/** Policies *******************************************************************************************************/
+class vi : public SolverCreator {
+public:
+    virtual Policy *operator()(MapfEnv *env, float gamma) {
+        return new ValueIterationPolicy(env, gamma, "vi");
+    }
+};
+
+
+/** Constants *******************************************************************************************************/
+vector<vector<EnvCreator *>> env_creators(
+        {   /* lvl 0 */
+                {new EmptyGrid("empty_8X8_2_agents_large_goal", 8, 2, 100),
+                 new EmptyGrid("empty_8X8_2_agents", 8, 2, 0),
+                 new SymmetricalBottleneck("symmetrical_bottleneck", 0),
+                 new SymmetricalBottleneck("symmetrical_bottleneck_large_goal", 100)}
+        }
+);
+
+vector<vector<SolverCreator *>> solver_creators(
+        {   /* lvl 0 */
+                {new vi()}
+        }
+);
+
+void benchmark_solver_on_env(Policy *policy) {
+    policy->train();
+    TrainInfo *train_info = policy->get_train_info();
+    EvaluationInfo *eval_info = policy->evaluate(100, 1000, 0);
 
     std::cout << "MDR:" << eval_info->mdr << " rate:" << eval_info->success_rate << " train_time:"
               << train_info->time;
-    std::cout << " exec_time:" << eval_info->mean_episode_time << " solver:" << policy.name;
+    std::cout << " exec_time:" << eval_info->mean_episode_time << " solver:" << policy->name;
 
     for (auto item: *train_info->additional_data) {
         std::cout << " " << item.first << ":" << item.second;
     }
 
     std::cout << endl;
+}
 
-    /* Just check out how much time it takes to iterate over the states */
-    MapfEnv *other_env = create_mapf_env("empty-8-8", 1, 2, 0.2, -1000, 0, -1);
+int main(int argc, char **argv) {
+    Policy *policy = nullptr;
+    MapfEnv *env = nullptr;
 
-    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    MultiAgentStateIterator s = other_env->observation_space->begin();
-    MultiAgentActionIterator a = other_env->action_space->begin();
-    std::stringstream sstream((*(train_info->additional_data))["n_iterations"]);
-    size_t n_iters;
-    sstream >> n_iters;
-    for (size_t i = 0; i < n_iters; ++i) {
-        for (s = other_env->observation_space->begin(); s != other_env->observation_space->end(); ++s) {
-            for (a = other_env->action_space->begin(); a != other_env->action_space->end(); ++a) {
-                for (Transition *t: *other_env->get_transitions(*s, *a)) {
-
+    for (size_t env_lvl = 0; env_lvl < env_creators.size(); ++env_lvl) {
+        for (size_t solver_lvl = env_lvl; solver_lvl < solver_creators.size(); ++solver_lvl) {
+            for (EnvCreator *env_creator: env_creators[env_lvl]) {
+                cout << env_creator->name << " :" << endl;
+                for (SolverCreator *solver_creator: solver_creators[solver_lvl]) {
+                    env = (*env_creator)();
+                    policy = (*solver_creator)(env, 1.0);
+                    benchmark_solver_on_env(policy);
                 }
             }
         }
     }
-
-    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    auto elapsed_time_milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
-    float elapsed_time_seconds = float(elapsed_time_milliseconds) / 1000;
-    elapsed_time_seconds = round(elapsed_time_seconds * 100) / 100;
-    cout << n_iters << " iterations took " << elapsed_time_seconds << " seconds" << endl;
 }
