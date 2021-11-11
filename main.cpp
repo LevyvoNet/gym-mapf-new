3129374eda05eb739b0c875c370a5e08e19ac68f
LevyvoNet <eladlevy2@gmail.com>
2021-11-11 09:10:16 +0200

    again, forgot to include RTDP

--- main.cpp
+++ main.cpp
@@ -171,7 +171,7 @@ vector<vector<SolverCreator *>> solver_creators(
 
                 /* lvl 1 */
                 {
-//                        new rtdp_dijkstra(),
+                        new rtdp_dijkstra(),
                 }
         }
 );
