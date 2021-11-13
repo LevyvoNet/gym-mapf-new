d2d755fc3f4e79a753c32c6b9e5a09fdd19a4185
LevyvoNet <eladlevy2@gmail.com>
2021-11-13 13:39:17 +0200

    another try after re-implementing dijkstra algorithm

--- solvers/heuristics/dijkstra_heuristic.cpp
+++ solvers/heuristics/dijkstra_heuristic.cpp
@@ -6,65 +6,57 @@
 
 
 /** Private *******************************************************************************/
-class LocationNode {
-public:
-    Location loc;
-    int distance;
-
-    LocationNode(Location loc, size_t distance) : loc(loc), distance(distance) {}
-};
-
-template<>
-class std::greater<LocationNode> {
-public:
-
-    bool operator()(const LocationNode &n1, const LocationNode &n2) {
-        return (n1.distance > n2.distance);
+size_t min_not_visited(int *distance, bool *visited, size_t size) {
+    size_t min_idx = 0;
+    int min_val = std::numeric_limits<int>::max();
+    for (size_t i = 0; i < size; ++i) {
+        if (!visited[i] && distance[i] < min_val) {
+            min_val = distance[i];
+            min_idx = i;
+        }
     }
-};
+
+    return min_idx;
+}
 
 void DijkstraHeuristic::dijkstra_single_agent(size_t agent_idx) {
     Location goal_location = this->env->goal_state->locations[agent_idx];
-    bool *visited = new bool[this->env->grid->id_to_loc.size()];
     int *d = this->distance[agent_idx];
-    std::priority_queue<LocationNode, vector < LocationNode>, std::greater<LocationNode>>
-    q;
     MultiAgentActionSpace single_action_space = MultiAgentActionSpace(1);
     std::vector<Location> neighbours;
     MultiAgentActionIterator a = single_action_space.begin();
     MultiAgentActionIterator action_space_end = single_action_space.end();
+    Location *curr_location = &goal_location;
+    int curr_location_id = curr_location->id;
+    unsigned long n_locs = this->env->grid->id_to_loc.size();
+    bool *visited = new bool[n_locs];
 
     /* Initialize visited to false */
-    std::fill(visited, visited + this->env->grid->id_to_loc.size(), false);
+    std::fill(visited, visited + n_locs, false);
 
     /* Start with the goal state */
     d[goal_location.id] = 0;
-    q.push(LocationNode(goal_location, 0));
 
-    while (q.size() > 0) {
-        if (q.size() % 100 == 0) {
-            std::cout << "size is " << q.size() << endl << std::flush;
-        }
+    for (size_t i = 1; i <= n_locs; ++i) {
+        curr_location_id = min_not_visited(d, visited, n_locs);
+        curr_location = this->env->grid->id_to_loc[curr_location_id];
 
-        /* Select the node with the minimum distance */
-        LocationNode curr_node = q.top();
-        q.pop();
-        visited[curr_node.loc.id] = true;
 
         /* Find the neighbours of the current node */
         neighbours.clear();
         for (a.reach_begin(); a != action_space_end; ++a) {
-            neighbours.push_back(this->env->grid->execute(curr_node.loc, a->actions[0]));
+            neighbours.push_back(this->env->grid->execute(*curr_location, a->actions[0]));
         }
 
         /* Update each neighbour and push it to the queue if we did not visit it yet */
         for (Location neighbour: neighbours) {
-            d[neighbour.id] = min(curr_node.distance + 1, d[neighbour.id]);
-            if (!visited[neighbour.id]) {
-                q.push(LocationNode(neighbour, d[neighbour.id]));
-            }
+            d[neighbour.id] = min(d[curr_location_id] + 1, d[neighbour.id]);
         }
+
+        visited[curr_location_id] = true;
     }
+l_cleanup:
+    delete[] visited;
 }
 
 
