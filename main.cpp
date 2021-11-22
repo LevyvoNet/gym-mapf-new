7d76b60b52f5df17558435d484b7125e11d2e677
LevyvoNet <eladlevy2@gmail.com>
2021-11-22 16:47:24 +0200

    oopsie

--- main.cpp
+++ main.cpp
@@ -172,7 +172,7 @@ vector<vector<SolverCreator *>> solver_creators(
                 /* lvl 1 */
                 {
 //                        new rtdp_dijkstra(),
-                        new id_rtdp()
+                        new id_rtdp(),
                         new id_rtdp_default(),
                 }
         }
