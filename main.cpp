a872c79cf1d9e5f8dee300bd65319a9fa3d78e7d
LevyvoNet <eladlevy2@gmail.com>
2021-11-23 11:52:43 +0200

    checking time measuring on CI

--- main.cpp
+++ main.cpp
@@ -176,8 +176,8 @@ vector<vector<SolverCreator *>> solver_creators(
 
                 /* lvl 1 */
                 {
-//                        new id_rtdp_default(),
-//                        new id_rtdp(),
+                        new id_rtdp_default(),
+                        new id_rtdp(),
                         new rtdp_dijkstra(),
                 },
                 /* lvl 2 */
