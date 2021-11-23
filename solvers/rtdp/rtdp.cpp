2abcbe3a3424dbf7dd24e21a21ef680f8383c7fc
LevyvoNet <eladlevy2@gmail.com>
2021-11-23 08:30:36 +0200

    rtdp without success rate epsilon check, testing on CI

--- solvers/rtdp/rtdp.cpp
+++ solvers/rtdp/rtdp.cpp
@@ -74,9 +74,9 @@ bool should_stop(EvaluationInfo *prev_eval_info, EvaluationInfo *curr_eval_info)
         return false;
     }
 
-    if ((std::abs(curr_eval_info->mdr - prev_eval_info->mdr) / std::abs(prev_eval_info->mdr)) >= MDR_EPSILON) {
-        return false;
-    }
+//    if ((std::abs(curr_eval_info->mdr - prev_eval_info->mdr) / std::abs(prev_eval_info->mdr)) >= MDR_EPSILON) {
+//        return false;
+//    }
 
     return true;
 }
