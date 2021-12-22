623d81aa5690929c592e273027dc244443dc6e68
LevyvoNet <eladlevy2@gmail.com>
2021-12-22 18:27:57 +0200

    another shot with berlin

--- main.cpp
+++ main.cpp
@@ -177,7 +177,7 @@ public:
     size_t n_agents;
     size_t scen_id;
 
-    BerlinEnv(string name, size_t n_agents, size_t scen_id) : EnvCreator(name), n_agents(n_agents), scen_id(scen_id) {}
+    BerlinEnv(string name, size_t scen_id, size_t n_agents) : EnvCreator(name), n_agents(n_agents), scen_id(scen_id) {}
 
     virtual MapfEnv *operator()() {
         return create_mapf_env("Berlin_1_256", this->scen_id, this->n_agents, 0.21, -1000, 0, -1);
@@ -271,9 +271,12 @@ vector<vector<EnvCreator *>> env_creators(
 //                        new MazeEnv("maze-32-32-4_scen_2_2-agents", 32, 4, 2, 2),
 //                        new MazeEnv("maze-32-32-4_scen_2_3-agents", 32, 4, 2, 3),
 //                        new MazeEnv("maze-32-32-4_scen_2_4-agents", 32, 4, 2, 4),
-                        new RoomEnv("room-64-64-8-scen_1_2-agents", 64, 8, 1, 2),
-                        new RoomEnv("room-64-64-8-scen_1_3-agents", 64, 8, 1, 3),
-                        new RoomEnv("room-64-64-8-scen_1_4-agents", 64, 8, 1, 4),
+//                        new RoomEnv("room-64-64-8-scen_1_2-agents", 64, 8, 1, 2),
+//                        new RoomEnv("room-64-64-8-scen_1_3-agents", 64, 8, 1, 3),
+//                        new RoomEnv("room-64-64-8-scen_1_4-agents", 64, 8, 1, 4),
+                        new BerlinEnv("berlin_scen_2_2-agents", 2, 2),
+                        new BerlinEnv("berlin_scen_2_3-agents", 2, 3),
+                        new BerlinEnv("berlin_scen_2_4-agents", 2, 4),
 
                 },
                 /* lvl 4 */
