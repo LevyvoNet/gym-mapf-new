c8eddc420a4ded4c8beb62c0fc7ad46a7c21d864
LevyvoNet <eladlevy2@gmail.com>
2022-01-03 19:08:19 +0200

    with online

--- main.cpp
+++ main.cpp
@@ -102,10 +102,10 @@ vector<vector<SolverCreator *>> solver_creators(
                 /* lvl 4 */
                 {
                         new dijkstra_baseline("dijkstra_baseline"),
-//                        new online_replan("online_replan_rtdp_2", 2, new rtdp_dijkstra_rtdp("")),
-//                        new online_replan("online_replan_rtdp_3", 3, new rtdp_dijkstra_rtdp("")),
-//                        new online_replan("online_replan_dijkstra_2", 2, new dijkstra_baseline("")),
-//                        new online_replan("online_replan_dijkstra_3", 3, new dijkstra_baseline("")),
+                        new online_replan("online_replan_rtdp_2", 2, new rtdp_dijkstra_rtdp("")),
+                        new online_replan("online_replan_rtdp_3", 3, new rtdp_dijkstra_rtdp("")),
+                        new online_replan("online_replan_dijkstra_2", 2, new dijkstra_baseline("")),
+                        new online_replan("online_replan_dijkstra_3", 3, new dijkstra_baseline("")),
                 }
         }
 );
