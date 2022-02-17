eff3d055c79ac12984d43f8dcf6c3c27542cde9a
LevyvoNet <eladlevy2@gmail.com>
2022-02-17 14:44:44 +0200

    fixed test

--- gym_mapf/tests/utils_tests.cpp
+++ gym_mapf/tests/utils_tests.cpp
@@ -12,11 +12,11 @@ TEST(UtilsTests, CreateMapfEnv) {
 
     ASSERT_EQ(*empty_8_8_1->s,
               *empty_8_8_1->locations_to_state(
-                      {empty_8_8_1->grid->get_location(0, 0), empty_8_8_1->grid->get_location(5, 3)}));
+                      {empty_8_8_1->grid->get_location(0, 0), empty_8_8_1->grid->get_location(3, 5)}));
 
     MapfEnv *empty_48_48_16 = create_mapf_env("empty-48-48", 16, 2, 0.2, -1000, 100, -1);
 
     ASSERT_EQ(*empty_48_48_16->s,
               *empty_48_48_16->locations_to_state(
-                      {empty_48_48_16->grid->get_location(40, 42), empty_48_48_16->grid->get_location(17, 2)}));
+                      {empty_48_48_16->grid->get_location(42, 40), empty_48_48_16->grid->get_location(2, 17)}));
 }
