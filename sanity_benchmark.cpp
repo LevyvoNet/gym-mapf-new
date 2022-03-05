4a61fd007bd8f8a376e293719311c13189297bb1
LevyvoNet <eladlevy2@gmail.com>
2022-03-05 21:43:11 +0200

    more benchmarkiung

--- sanity_benchmark.cpp
+++ sanity_benchmark.cpp
@@ -57,16 +57,16 @@ vector<vector<EnvCreator *>> env_creators(
 //                        new BerlinEnv("paris_1_256_scen_2_4-agents", 2, 4),
 
                         /* Dragon Age */
-                        new GeneralEnv("ost003d_scen_10_8-agents", "ost003d", 10, 8),
+                        new GeneralEnv("ost003d_scen_7_8-agents", "ost003d", 7, 8),
 
                         /* Open */
-                        new GeneralEnv("empty-16-16_scen_1_6-agents", "empty-16-16", 1, 6),
+                        new GeneralEnv("empty-48-48_scen_1_6-agents", "empty-48-48", 1, 5),
 
                         /* Open + obstacles */
-                        new GeneralEnv("random-64-64-10_scen_14_4-agents", "random-64-64-10", 10, 8),
+                        new GeneralEnv("random-64-64-10_scen_14_4-agents", "random-64-64-10", 10, 5),
 
                         /* Maze */
-                       new MazeEnv("maze-32-32-4-scen_16_8-agents", 32, 4, 2, 8),
+                       new MazeEnv("maze-32-32-4-scen_1_5-agents", 32, 4, 1, 5),
 
                         /* Room */
                         new GeneralEnv("room-64-64-16_scen_1_8-agents", "room-64-64-16", 1, 8),
