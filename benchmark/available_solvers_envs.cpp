e8400b596999673bc510d53bce54bd0f6e69c164
LevyvoNet <eladlevy2@gmail.com>
2022-03-18 10:02:41 +0200

    almost the same as symmetrical

--- benchmark/available_solvers_envs.cpp
+++ benchmark/available_solvers_envs.cpp
@@ -68,7 +68,7 @@ MapfEnv *PaperExample::operator()() {
                                           "..@...",
                                           "..@...",
                                           "..@...",
-                                          "......",
+                                          "..@...",
                                   });
 
     Grid *g = new Grid(map_lines);
@@ -81,8 +81,8 @@ MapfEnv *PaperExample::operator()() {
 //                               g->get_location(5, 0)
                        },
                        {
-                               g->get_location(0, 5),
-                               g->get_location(0, 0),
+                               g->get_location(1, 5),
+                               g->get_location(1, 0),
 //                               g->get_location(5, 5)
                        },
                        FAIL_PROB,
