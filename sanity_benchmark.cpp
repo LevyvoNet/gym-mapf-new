64951bf3f7183c216ad0df79daadd710a9977561
LevyvoNet <eladlevy2@gmail.com>
2022-04-05 14:27:54 +0300

    comparing similar envs

--- sanity_benchmark.cpp
+++ sanity_benchmark.cpp
@@ -61,8 +61,7 @@ vector<vector<EnvCreator *>> env_creators(
 //                        new BerlinEnv("paris_1_256_scen_2_4-agents", 2, 4),
 
                         /* Dragon Age */
-//                        new GeneralEnv("", "ost003d", 5, 4),
-//                        new GeneralEnv("", "ost003d", 4, 4),
+                        new GeneralEnv("", "ost003d", 4, 4),
 
                         /* Open */
 //                        new GeneralEnv("empty-48-48_scen_1_4-agents", "empty-48-48", 1, 4),
@@ -74,6 +73,7 @@ vector<vector<EnvCreator *>> env_creators(
 //                       new MazeEnv("", 128, 10, 1, 5),
 
                         /* Room */
+                        new GeneralEnv("", "room-64-64-16", 1, 6),
                         new GeneralEnv("", "room-64-64-16", 1, 8),
                 }
         }
