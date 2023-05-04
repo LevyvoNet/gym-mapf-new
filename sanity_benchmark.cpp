3b5914df5132f31a12ba264250bd8e7ad65cbc2e
LevyvoNet <eladlevy2@gmail.com>
2023-05-04 17:30:25 +0300

    RTDP parameters had nothing to do with it, returned them to what they were. Now trying harder envs to see if there is a difference

--- sanity_benchmark.cpp
+++ sanity_benchmark.cpp
@@ -41,10 +41,10 @@ vector<vector<EnvCreator *>> env_creators(
                 },
                 /* lvl 1 */
                 {
-//                        new RoomEnv("room-32-32-4_scen-12_2-agents", 32, 4, 12, 2),
-//                        new SanityEnv("independent_8X8_3-agents", 3, 8, 3),
-//                        new EmptyGrid("empty_16X16_2-agents", 16, 2, 0),
-//                        new EmptyGrid("empty_16X16_2-agents_large_goal", 16, 2, 100)
+                        new RoomEnv("room-32-32-4_scen-12_2-agents", 32, 4, 12, 2),
+                        new SanityEnv("independent_8X8_3-agents", 3, 8, 3),
+                        new EmptyGrid("empty_16X16_2-agents", 16, 2, 0),
+                        new EmptyGrid("empty_16X16_2-agents_large_goal", 16, 2, 100)
                 },
                 /* lvl 2 */
                 {
