0548bea9dae89ecc878b8cf8992daca74f20d6c2
LevyvoNet <eladlevy2@gmail.com>
2023-05-08 12:08:53 +0300

    more envs for sanity on online_window_vi

--- sanity_benchmark.cpp
+++ sanity_benchmark.cpp
@@ -62,20 +62,20 @@ vector<vector<EnvCreator *>> env_creators(
 //                        new BerlinEnv("paris_1_256_scen_2_4-agents", 2, 4),
 
                         /* Dragon Age */
-//                        new GeneralEnv("", "ost003d", 4, 4),
+                        new GeneralEnv("", "ost003d", 4, 4),
 
                         /* Open */
-//                        new GeneralEnv("empty-48-48_scen_1_4-agents", "empty-48-48", 1, 4),
+                        new GeneralEnv("empty-48-48_scen_1_4-agents", "empty-48-48", 1, 4),
 
                         /* Open + obstacles */
-//                        new GeneralEnv("", "random-64-64-10", 1, 4),
+                        new GeneralEnv("", "random-64-64-10", 1, 4),
 
                         /* Maze */
 //                       new MazeEnv("", 128, 10, 1, 5),
 
                         /* Room */
-//                        new GeneralEnv("", "room-64-64-16", 1, 6),
-//                        new GeneralEnv("", "room-64-64-16", 1, 8),
+                        new GeneralEnv("", "room-64-64-16", 1, 6),
+                        new GeneralEnv("", "room-64-64-16", 1, 8),
                 }
         }
 );
